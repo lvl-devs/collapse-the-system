8903b0160b205bb9c2f0de9a6e1dcf131c3bc1b8
Pako3549 <bellarosa.pasquale@gmail.com>
2026-03-12 10:10:13 +0100

    fix: update loading scene to start Minigame1 and refactor input handling in Minigame1

--- src/game/scenes/Boot.ts
+++ src/game/scenes/Boot.ts
@@ -17,9 +17,9 @@ export default class Boot extends Phaser.Scene {
 
   create(){
     this._loadingText.setText(GameData.preloader.loadingTextComplete);
-    this.input.keyboard!.on("keydown", () => {
+    this.input.keyboard!.once("keydown", () => {
         this.scene.stop(this);
-        this.scene.start("Minigame2");
+        this.scene.start("Minigame1");
     });
   }
 
